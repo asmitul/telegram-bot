cf75032319c4490305a1a6bef879a52d638db2f3
gabrial-dagon <117000910+malwina-tex@users.noreply.github.com>
2023-02-15 20:21:53 +0800

    Create telegram.Message.reply_text().py

---
+++ example/send messages/telegram.Message.reply_text().py
@@ -0,0 +1,47 @@
+# python 3.8
+# python-telegram-bot 20.1
+
+from telegram import Chat, ChatMember, ChatMemberUpdated, Update
+from telegram.constants import ParseMode
+from telegram.ext import (
+    Application,
+    ChatMemberHandler,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    await update.message.reply_text("Hi")
+    await update.effective_message.reply_text(f"Hi, second way to send msg.")
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    # replace you bot_token in .env
+    application = Application.builder().token(
+        BOT_TOKEN).build()
+
+    # Interpret any other command or text message as a start of a private chat.
+    # This will record the user as being in a private chat with bot.
+    application.add_handler(MessageHandler(filters.ALL, start))
+
+    # Run the bot until the user presses Ctrl-C
+    # We pass 'allowed_updates' handle *all* updates including `chat_member` updates
+    # To reset this, simply pass `allowed_updates=[]`
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
